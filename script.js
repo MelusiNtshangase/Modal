a5eb690b366e9fd1ce235b2268541fb33d8e8607
Melusi Ntshangase <mentsha022@student.wethinkcode.co.za>
2023-07-31 13:04:40 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', function () {
+    console.log('button clicked');
+
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    closeModal();
+  }
+});
